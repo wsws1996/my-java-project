82fd63e60c3e03806a095dc25efeea9a1101f369
wang <wang@hn.kd.ny.adsl>
2015-12-22 22:07:01 +0800

    第十六天进行中

---
+++ workspace/day16/src/Account.sql
@@ -0,0 +1,16 @@
+use database day16;
+create table account
+(
+   id int primary key auto_increment, name varchar(40), money float
+)
+character set utf8 collate utf8_general_ci
+;insert into account 
+(name ,money) VALUES
+('aaa',1000 )
+;insert into account 
+(name ,money) VALUES
+('bbb',1000 )
+;insert into account 
+(name ,money) VALUES
+('ccc',1000 )
+;
