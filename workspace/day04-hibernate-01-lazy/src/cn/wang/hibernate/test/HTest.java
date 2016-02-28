8d64ff06879b2c44f02de8dafb4fe74bca879cb2
wsws1996 <wang@localhost.localdomain>
2016-02-28 22:58:38 +0800

    第四天进行中

--- workspace/day04-hibernate-01-lazy/src/cn/wang/hibernate/test/HTest.java
+++ workspace/day04-hibernate-01-lazy/src/cn/wang/hibernate/test/HTest.java
@@ -2,6 +2,7 @@ package cn.wang.hibernate.test;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -9,30 +10,16 @@ import org.hibernate.Transaction;
 import org.junit.Test;
 
 import cn.wang.hibernate.domain.Classes;
+import cn.wang.hibernate.domain.Student;
 import cn.wang.hibernate.utils.HibernateUtils;
 
 public class HTest {
 	@Test
 	public void test() throws InterruptedException {
 		Session session = HibernateUtils.sessionFactory.openSession();
-		Query query = session.createQuery("select name from Classes");
-		query.setCacheable(true);
-		query.list();
-		session.close();
-
-		session = HibernateUtils.sessionFactory.openSession();
-
-		Transaction transaction = session.beginTransaction();
 		Classes classes = (Classes) session.get(Classes.class, 1L);
-		classes.setName("us");
-
-		transaction.commit();
-		session.close();
-
-		session = HibernateUtils.sessionFactory.openSession();
-		query = session.createQuery("select name from Classes");
-		query.setCacheable(true);
-		query.list();
+		Set<Student> students=classes.getStudents();
+		System.out.println(students.size());
 		session.close();
 	}
 }
