e1195c025e3bb0f808fb8e460212e1378f48fb72
pad <pad>
2016-03-04 10:07:10 +0800

    aop1 finished

--- workspace/day02-spring/src/cn/wang/spring/xml/transaction/PersonDaoImpl.java
+++ workspace/day02-spring/src/cn/wang/spring/xml/transaction/PersonDaoImpl.java
@@ -3,8 +3,10 @@ package cn.wang.spring.xml.transaction;
 public class PersonDaoImpl implements PersonDao {
 
 	@Override
-	public void savePerson() {
+	public String savePerson() {
+//		int i = 1 / 0;
 		System.out.println("save person");
+		return "success";
 	}
 
 }
--- workspace/day02-spring/src/cn/wang/spring/xml/transaction/Transaction.java
+++ workspace/day02-spring/src/cn/wang/spring/xml/transaction/Transaction.java
@@ -1,10 +1,30 @@
 package cn.wang.spring.xml.transaction;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+
 public class Transaction {
-	public void beginTransaction() {
+	public void beginTransaction(JoinPoint joinpoint) {
+		String methodName = joinpoint.getSignature().getName();
+		System.out.println("methodname:" + methodName);
+		System.out.println("class:" + joinpoint.getTarget().getClass());
 		System.out.println("begin Transaction");
 	}
-	public void commit() {
+
+	public void commit(JoinPoint joinpoint, Object val) {
+		System.out.println("returning:" + val);
 		System.out.println("commit");
+	}
+
+	public void finallyMethod() {
+		System.out.println("finally method execute");
+	}
+
+	public void throwingMethod(JoinPoint joinPoint, Throwable throwable) {
+		System.out.println("message:" + throwable.getMessage());
+	}
+	public void aroundMethod(ProceedingJoinPoint point) throws Throwable {
+		System.out.println("pro");
+		point.proceed();
 	}
 }
