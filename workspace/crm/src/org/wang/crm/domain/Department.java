88d4aed641cecbe135c11fa396c5cda6fedb53b3
wsws1996 <wang@localhost.localdomain>
2016-03-09 21:15:26 +0800

    crming

---
+++ workspace/crm/src/org/wang/crm/domain/Department.java
@@ -0,0 +1,34 @@
+package org.wang.crm.domain;
+
+import java.io.Serializable;
+
+public class Department implements Serializable {
+	private Long did;
+	private String name;
+	private String description;
+
+	public Long getDid() {
+		return did;
+	}
+
+	public void setDid(Long did) {
+		this.did = did;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
