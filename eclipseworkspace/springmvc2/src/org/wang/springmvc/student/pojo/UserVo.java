127119891550fc034b57b2a82137fad6e13b52fc
wsws1996 <wang@localhost.localdomain>
2016-05-07 23:32:11 +0800

    springmvc进行中

---
+++ eclipseworkspace/springmvc2/src/org/wang/springmvc/student/pojo/UserVo.java
@@ -0,0 +1,14 @@
+package org.wang.springmvc.student.pojo;
+
+public class UserVo {
+	private Student student;
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+}
