48700fe2d64971a15a6c951aa1d1803e5a45012b
wsws1996 <1786578565@qq.com>
2016-11-18 20:52:33 +0800

    15-jedis连接集群

--- eclipseworkspace/jedis/src/com/wang/jedis/client/JedisClient.java
+++ eclipseworkspace/jedis/src/com/wang/jedis/client/JedisClient.java
@@ -1,15 +1,20 @@
 package com.wang.jedis.client;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.Test;
 
+import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
 import redis.clients.jedis.JedisPool;
 
 public class JedisClient {
 	@Test
 	public void jedisClient() {
 		// jedis
-		Jedis jedis = new Jedis("10.42.0.154", 6379);
+		Jedis jedis = new Jedis("127.0.0.1", 6379);
 		// 通过redis赋值
 		jedis.set("s2", "222");
 		// 通过redis取值
@@ -22,7 +27,7 @@ public class JedisClient {
 	@Test
 	public void jedisPool() {
 		// jedisPool
-		JedisPool pool = new JedisPool("10.42.0.154", 6379);
+		JedisPool pool = new JedisPool("127.0.0.1", 6379);
 		// 通过连接池获取jedis对象
 		Jedis jedis = pool.getResource();
 
@@ -31,5 +36,18 @@ public class JedisClient {
 		System.out.println(result);
 		jedis.close();
 		pool.close();
+	}
+
+	@Test
+	public void jedisCluster() {
+		// 创建jedisCluster
+		Set<HostAndPort> nodes = new HashSet<HostAndPort>();
+		nodes.add(new HostAndPort("127.0.0.1", 6379));
+		JedisCluster cluster = new JedisCluster(nodes);
+
+		cluster.set("s4", "444");
+		String result = cluster.get("s4");
+		System.out.println(result);
+		cluster.close();
 	}
 }
