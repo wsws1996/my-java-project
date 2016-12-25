0e1500f1c63c0288119eae29efdd1b204f1bee55
wsws1996 <1786578565@qq.com>
2016-12-25 13:57:17 +0800

    期末小项目

--- eclipseworkspace/ProcessFinalExaminationData/src/Process.java
+++ eclipseworkspace/ProcessFinalExaminationData/src/Process.java
@@ -1,24 +1,31 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Process {
 
 	public static void main(String[] args) throws IOException {
-		File file = new File("/home/wang/桌面/计算机网络(谢希仁版)复习资料整理(期末考试必备).txt");
+		File file = new File("/home/wang/桌面/tmp/微机原理.txt");
 		Scanner scanner = new Scanner(file);
-		FileWriter fileWriter = new FileWriter("/home/wang/桌面/计算机网络(谢希仁版)复习资料整理(期末考试必备).txtbyjava");
+		FileWriter fileWriter = new FileWriter("/home/wang/桌面/tmp/微机原理.txtbyjava");
 		while (scanner.hasNextLine()) {
 			String nextLine = scanner.nextLine();
 			if (nextLine.length() > 0) {
 				String replace = nextLine.replace(" ", "");
+				List<Character> asList = Arrays.asList('，', '。', '；');
+				if (!asList.contains(replace.charAt(replace.length() - 1))) {
+					replace = replace + "；";
+				}
 				StringBuilder stringBuilder = new StringBuilder(replace);
-//				if (stringBuilder.indexOf("第") != -1 && stringBuilder.indexOf("页") != -1
-//						&& stringBuilder.indexOf("共") != -1) {
-//					stringBuilder.delete(0, 6);
-//					System.out.println(stringBuilder.toString());
-//				}
+				// if (stringBuilder.indexOf("第") != -1 &&
+				// stringBuilder.indexOf("页") != -1
+				// && stringBuilder.indexOf("共") != -1) {
+				// stringBuilder.delete(0, 6);
+				// System.out.println(stringBuilder.toString());
+				// }
 				fileWriter.write(stringBuilder + "\n");
 			}
 		}
